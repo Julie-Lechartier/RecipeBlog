03ab7edaac2b0ccf706ad34c3587a3ee3e7bf64b
julie <julie.lechartier@gmail.com>
2025-04-29 09:04:24 +0200

    [MODIFY] add bannière change

---
+++ src/Entity/Banner.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BannerRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BannerRepository::class)]
+class Banner
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    /**
+     * @var Collection<int, media>
+     */
+    #[ORM\ManyToMany(targetEntity: media::class, inversedBy: 'banners')]
+    private Collection $image;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $link = null;
+
+    #[ORM\Column]
+    private ?bool $button = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $buttonText = null;
+
+    #[ORM\Column]
+    private ?bool $isActive = null;
+
+    public function __construct()
+    {
+        $this->image = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, media>
+     */
+    public function getImage(): Collection
+    {
+        return $this->image;
+    }
+
+    public function addImage(media $image): static
+    {
+        if (!$this->image->contains($image)) {
+            $this->image->add($image);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(media $image): static
+    {
+        $this->image->removeElement($image);
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(?string $link): static
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function isButton(): ?bool
+    {
+        return $this->button;
+    }
+
+    public function setButton(bool $button): static
+    {
+        $this->button = $button;
+
+        return $this;
+    }
+
+    public function getButtonText(): ?string
+    {
+        return $this->buttonText;
+    }
+
+    public function setButtonText(?string $buttonText): static
+    {
+        $this->buttonText = $buttonText;
+
+        return $this;
+    }
+
+    public function isActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): static
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+}
---
+++ src/Form/BannerType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Banner;
+use App\Entity\media;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BannerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('link')
+            ->add('button')
+            ->add('buttonText')
+            ->add('isActive')
+            ->add('image', EntityType::class, [
+                'class' => media::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Banner::class,
+        ]);
+    }
+}

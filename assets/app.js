be62858a4e3820ab8fe29c840b9fe41ae9ab936a
[Julie Lechartier] <bourg.juju@gmail.com>
2025-05-03 13:34:50 +0200

    modify design color and RegistrationController.php

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,17 @@
+// assets/js/app.js
+// Importer les styles
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'select2/dist/css/select2.min.css';
+import 'select2-bootstrap-5-theme';
+
+// Importer jQuery et le rendre global
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+// Importer Bootstrap
 import * as bootstrap from 'bootstrap';
 window.bootstrap = bootstrap;
+
+// Importer Select2
+import 'select2';

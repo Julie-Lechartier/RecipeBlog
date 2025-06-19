92bdb42bbb67a706f364acf4f9d3ad92a121017f
julie <julie.lechartier@gmail.com>
2025-06-19 15:04:44 +0200

    [MODIFY] push

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 #[Route('/admin')]
 class AdminController extends AbstractController
 {
